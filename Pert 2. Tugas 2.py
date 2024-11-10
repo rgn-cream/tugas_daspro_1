0971a9e7a572474bc9f0b5417b03be81c2d5dd9b
rgn-cream <reginanf7@gmail.com>
2024-11-10 16:51:19 +1200

    Mengubah variabel tahun lahir

--- Pert 2. Tugas 2.py
+++ Pert 2. Tugas 2.py
@@ -3,9 +3,9 @@ NIM : 2406043
 Kelas : 1B"""
 
 nama = input("Nama: ")
-tahunlahir = int(input("Tahun lahir: "))
+tahun_lahir = int(input("Tahun lahir: "))
 tahun_ini = 2024 
-umur =tahun_ini - int(tahunlahir)
+umur =tahun_ini - int(tahun_lahir)
 
 print(f"Selamat datang, {nama}! Umur Anda sekarang adalah {umur}")
 
